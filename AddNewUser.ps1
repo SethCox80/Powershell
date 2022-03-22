f1aa6e6630c6127c5f6d14cafd1c5d66a6012e0f
Seth Cox <seth.cox80@protonmail.com>
2022-03-22 09:52:34 -0400

    Edits for functions and added some regex

--- AddNewUser.ps1
+++ AddNewUser.ps1
@@ -1,191 +1,198 @@
 #
-Import-Module -name MSOnline
+# if (($PSVersionTable.PSVersion).Major -gt 5){Powershell}
+# Pause
+# Import-Module -name MSOnline
+# Connect-Msolservice
 
-#Global
-$ValAns = ("y", "Y", "n", "N")
-$Grade = ""
+#Functions
 
-#Funcions
-function FirstMenu() {
-    Clear-Host
-    WRite-host "****************************************************************************************************"
-    Write-Host "*       Create a new User:                                                                         *"
-    Write-Host "*       1. Student Account                                                                         *"
-    Write-Host "*       2. Employee Account                                                                        *"
-    Write-Host "*                                                                                                  *"
-    WRite-host "****************************************************************************************************"
+#  “^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()!])(?=\\S+$).{8, 20}$”
+#  "^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W)$"
+
+#******************************************************************************************************
+function YesorNo() {
+    do {
+        $Ans = (read-host "Y or N").ToUpper()         
+        if ($Ans -match "^Y|N$") {
+            $Valid = $true
+        }
+        else {
+            write-host "You answered '$Ans'. Please Enter " -ForegroundColor Red -NoNewline
+            $Valid = $false
+        }
+    }while ($Valid -eq $false)
+    if ($Ans -eq "Y") {
+        return $true
+    }
+    else {
+        return $false
+    }
 }
-
+#******************************************************************************************************
+function ValidateName {
+    param (
+        [Parameter()]
+        [String]
+        $NameToCheck
+    )
+    if ($NameToCheck -match "^[A-Za-z]+$") {
+        # Will retrun only if the Name is Valid - No numbers, spaces, or special characters        
+        return $NameToCheck
+    }
+    Else {
+        write-host "Invalid name. No spaces, numbers, or special characters. " -NoNewline
+        ValidateName (read-Host "Please Enter a valid name ")
+    }    
+}
+#******************************************************************************************************
+function MainMenu() {
+    WRite-host "*******************************************************************" -ForegroundColor Green
+    Write-Host "*       Create a new User:                                        *" -ForegroundColor Green
+    Write-Host "*       1. Student Account                                        *" -ForegroundColor Green
+    Write-Host "*       2. Employee Account                                       *" -ForegroundColor Green
+    Write-Host "*       3. End Program (Answer 'N' to 'Create another User?')     *" -ForegroundColor Green
+    Write-Host "*                                                                 *" -ForegroundColor Green
+    WRite-host "*******************************************************************" -ForegroundColor Green
+}
+#******************************************************************************************************
 function ConfirmInfo {
     #Confirm Information
     Write-Host "-------------------------------------------------------------------"
     if ($NewOpt -eq 1) { Write-Host "Student's name to be added:"$First, $Last -ForegroundColor Yellow } #If new user is student
-    else { Write-Host "Employee's name to be added:"$First, $Last -ForegroundColor Yellow } #if new user is emplyee
+    else { Write-Host "Employee's name to be added:"$First, $Last -ForegroundColor Yellow } #if new user is employee
     Write-Host "Display Name: "$DisplayNm -ForegroundColor Blue
-    if ($Grade -ne "") { Write-Host "Grade of new Student"$Grade -ForegroundColor Yellow } #Should skip this print line if Employee
-    Write-Host "Principal User Name: "$PrincipalUserName -ForegroundColor Yellow
+    if (!$IsEmp) { Write-Host "Grade of new Student"$Grade -ForegroundColor Green } #Should skip this print line if Employee
+    Write-Host "Principal User Name: "$PrincUserName -ForegroundColor Blue
+    If (!$IsEmp) { Write-Host "Password will be entered at next Prompt" -ForegroundColor Red }
+    elseif ($IsEmp) { Write-Host "Password set to 'Start1234' and force change at first login! " -ForegroundColor Red } 
     Write-Host "--------------------------------------------------------------------`n"
 }
-Do {
-    function NewStudent {
-        Do {
-            Clear-Host
-            FirstMenu #Call Menu
-            Write-Host "Enter Data"
-            #Get Student Name
-            $Last = Read-Host "What is new student's last name "
-            $First = Read-Host "What is new student's first name "
-            Write-Host "`nStudents name to be added:"$First, $Last -ForegroundColor Red
-
-            #Get Student Grade
-            Do {
-                $Grade = Read-Host "`nEnter Student's grade (4-8 use numeral)"
-                if (($Grade -ge 4) -and ($Grade -le 8)) {
-                    Write-host "Correct"
-                    $GradeCheck = $false
-                }
-                else {
-                    Write-host "Please enter a grade number 4-8" -ForegroundColor Red
-                    $GradeCheck = $true
-                }
-
-            } while ($GradeCheck) 
-
-            #Set User Values
-            Do {
-                $StudentNum = Read-Host "`nEnter Student number for new Student (####)"
-                if ($StudentNum -match "^\d{4}$") {
-                    #checks for 4 digits - numbers only
-                    $NumIsRight = $true
-                }
-                else {
-                    Write-host "Please enter a 4 digit number - numbers only"
-                    $NumIsRight = $false
-                }
-
-            } while ($StudentNum.Length -ne 4 -or $NumIsRight -eq $false )
-
-            $PrincipalUserName = $Last + $StudentNum.Substring(1, 3) + "@wwchristianschool.org"
-            $DisplayNm = $First, $Last
-            Switch ($Grade) {
-                4 { $Dept = "Fourth" }
-                5 { $Dept = "Fifth" }
-                6 { $Dept = "Sixth" }
-                7 { $Dept = "Seventh" }
-                8 { $Dept = "Eighth" }
-            }
-            ConfirmInfo
-            $AnsCheck = $true
-            while ($AnsCheck) {
-                #Will Loop until Valid answer is chosen
-                $Ans = Read-Host "Satisfied (Y or N)"
-                if ($Ans -in $ValAns) {
-                    $AnsCheck = $false #Will break Answer Check loop
-                }         
-                else {
-                    Write-Host "(Y or N) you endered '$Ans'"
-                }
-            }
-            if ($Ans -in ("Y", "y")) {
-                $DoLoop = $false
-            }
-            else {
-                $DoLoop = $true
-            } 
-        } while ($DoLoop)
-
-        Pause
+#******************************************************************************************************
+function NewStudent {
+    Do {
+        $IsEmp = $false
         Clear-Host
-
-        FirstMenu #Call Menu
-        $PassWD = Read-Host "Please enter tha password for $DisplayNm "
-        $Force = $false
-        # New-MsolUser `
-        #     -DisplayName "$DisplayNm" `
-        #     -FirstName "$First" `
-        #     -LastName "$Last" `
-        #     -UserPrincipalName "$PrincipalUserName" `
-        #     -Password "$PassWD" `
-        #     -ForceChangePassword $Force `
-        #     -Title "Student" `
-        #     -Department "$Dept" `
-        #     -LicenseAssignment "wwchristianschoolorg:ENTERPRISEPACKPLUS_STUUSEBNFT" -UsageLocation US
-
-        Pause
-    }
-    function NewEmployee {
+        MainMenu #Call Menu
+        #Get Student Name
+        Write-Host "Please enter name with no spaces, numbers, or characters."
+        $First = ValidateName(read-Host "Please enter first name ")
+        $Last = ValidateName(read-Host "Please enter last name ")
+        #Set StudentId Number for username formation
         Do {
-            Write-Host "Enter Data"
-            #Get Student Name
-            $Last = Read-Host "What is new student's last name "
-            $First = Read-Host "What is new student's first name "
-            $PrincipalUserName = $First.substring(0, 1) + $Last + "@wwchristianschool.org"
-            $DisplayNm = $First, $Last
-
-            FirstMenu
-            ConfirmInfo
-
-            $AnsCheck = $true
-            while ($AnsCheck) {
-                #Will Loop until Valid answer is chosen
-                $Ans = Read-Host "Satisfied with information and create user? (Y or N)"
-                if ($Ans -in $ValAns) {
-                    $AnsCheck = $false #Will break Answer Check loop
-                }         
-                else {
-                    Write-Host "(Y or N) you endered '$Ans'"
-                }
-            }
-            if ($Ans -in ("Y", "y")) {
-                $DoLoop = $false
+            $StudentNum = Read-Host "Enter Student number for new Student (####)"
+            if ($StudentNum -match "^\d{4}$") {
+                #checks for 4 digits - numbers only
+                $NumIsRight = $true
             }
             else {
-                $DoLoop = $true
-            } 
-        } while ($DoLoop)
-        $StTitle = "Teacher"
-        # $Force = $true
-        # New-MsolUser `
-        #     -DisplayName "$DisplayNm" `
-        #     -FirstName "$First" `
-        #     -LastName "$Last" `
-        #     -UserPrincipalName "$PrincipalUserName" `
-        #     -Password "Start!1234" `
-        #     -ForceChangePassword $Force `
-        #     -Title "$StTitle" `
-        #     -Department "$Dept" `
-        #     -LicenseAssignment "wwchristianschoolorg:STANDARDWOFFPACK_FACULTY" -UsageLocation US
+                Write-host "Please enter a 4 digit number - numbers only"
+                $NumIsRight = $false
+            }
+        } while (!$NumIsRight)
+        #Get Student Grade
+        Do {
+            $Grade = Read-Host "Enter Student's grade (4-8 use numeral)" #Only 4-8 grade have ACTIVE AND UNLOCKED O365 Accounts>
+            if ($Grade -match "^[4-8]{1}$") {
+                $GradeCheck = $true
+            }
+            else {
+                Write-host "Please enter a grade number 4-8" -ForegroundColor Red
+                $GradeCheck = $false
+            }
+        } while (!$GradeCheck) 
+        $PrincUserName = $Last + $StudentNum.Substring(1, 3) + "<@EmailDomain>"
+        $DisplayNm = $First, $Last
+        Switch ($Grade) {
+            #Dept is used for the grade in Student contact info
+            4 { $Dept = "Fourth" }
+            5 { $Dept = "Fifth" }
+            6 { $Dept = "Sixth" }
+            7 { $Dept = "Seventh" }
+            8 { $Dept = "Eighth" }
+        }
+        ConfirmInfo
+        write-host "Is this correct? " -NoNewline
+        $Ans = YesorNo
+        return $IsEmp
+    } while (!$Ans)
+    Clear-Host
+    MainMenu #Call Menu
+    $PassWD = Read-Host "Please enter Desired password for $DisplayNm "
+    $Force = $false
+    # New-MsolUser `
+    #     -DisplayName "$DisplayNm" `
+    #     -FirstName "$First" `
+    #     -LastName "$Last" `
+    #     -UserPrincipalName "$PrincUserName" `
+    #     -Password "$PassWD" `
+    #     -ForceChangePassword $Force `
+    #     -Title "Student" `
+    #     -Department "$Dept" `
+    #     -LicenseAssignment "wwchristianschoolorg:ENTERPRISEPACKPLUS_STUUSEBNFT" -UsageLocation US
+     
+}
+#******************************************************************************************************
+function NewEmployee {
+    Do {
+        #Get Employee Name
+        $IsEmp = $true
+        Clear-Host
+        #Call Menu   
+        MainMenu
+        #Get Employee Name         
+        Write-Host "Please enter name with no spaces, numbers, or characters."
+        $First = ValidateName(read-Host "Please enter first name ")
+        $Last = ValidateName(read-Host "Please enter last name ")
+        $PrincUserName = $First.substring(0, 1) + $Last + "@wwchristianschool.org"
+        $DisplayNm = $First, $Last
+        Clear-Host
+        MainMenu
+        ConfirmInfo
+        write-host "Is this correct? " -NoNewline
+        $Ans = YesorNo
+    } while (!$Ans)
+    $StTitle = "Teacher"
+    $Force = $true
+    # New-MsolUser `
+    #     -DisplayName "$DisplayNm" `
+    #     -FirstName "$First" `
+    #     -LastName "$Last" `
+    #     -UserPrincipalName "$PrincUserName" `
+    #     -Password "Start!1234" `
+    #     -ForceChangePassword $Force `
+    #     -Title "$StTitle" `
+    #     -Department "$Dept" `
+    #     -LicenseAssignment "wwchristianschoolorg:STANDARDWOFFPACK_FACULTY" -UsageLocation US
+     
+}
+#******************************************************************************************************
 
-        Pause
-    
-    }
-
-    #Start Porgram
-
-    FirstMenu #Call Menu
-
-    $NewOpt = read-host "`n`nPlease Select and option 1 or 2"
+#Start Porgram
+Do {
+    #Call Menu
+    do {
+        #ensures valid option
+        Clear-Host
+        MainMenu 
+        $NewOpt = read-host "Please Select and option 1 or 2 "
+        if ($NewOpt -match "^[1-3]{1}$") {
+            # Ensures option matches the pattern 1-2 and is expandable to any number
+            $ValidOpt = $true 
+        }
+        else {
+            write-host "You entered '$NewOpt' Please enter a vlaid option. " -ForegroundColor Red -NoNewline
+            Pause
+            $ValidOpt = $false
+        }
+    }while (!$ValidOpt)
     switch ($NewOpt) {
+        #Expandable with new functions added
         1 { NewStudent }
         2 { NewEmployee }
     }
-Do{
-$DoNew = read-host "And another User? (Y or N)"
-if ($DoNew -in $ValAns){
-    if ($DoNew -in ("Y","y")){
-        $AnsCheck = $true
-    }
-    else{
-        $AnsCheck = $false
-        Write-host "End Program"
-        Pause
-        Clear-Host
-    }
-}
-Else{
-    Write-Host
-}
-} while ($AnsCheck -eq $false)
-
-} while ($NewU)
-
+    Write-Host "Create another User? " -NoNewline
+    $Ans = YesorNo
+} while ($Ans)
+write-host "Ending Script" -ForegroundColor Red
+Pause
+clear-host
