b3334b8e0366a73c13e4c1aa765030824c4ea828
Seth Cox <seth.cox80@gmail.com>
2022-04-18 19:43:50 -0400

    Clean code in SetSMTP... script

--- SetSMTPPolicy.ps1
+++ SetSMTPPolicy.ps1
@@ -3,19 +3,18 @@ Import-Module MSOnline
 
 $Users = @{}
 $Auth = @{}
+$Dept = "Staff"
 
 $Users = Get-MsolUser
-
 $Count = 1
-
 foreach($User in $Users){
-    if($User.Department -eq "Staff"){
+    if($User.Department -eq $Dept){
         write-host $Count " "$User.Displayname -ForegroundColor Blue
-        Write-Host $User.Department
-        Write-Host $User.ObjectId
-        $Count = $Count + 1
+        Write-Host "Department: " $User.Department
+        Write-Host "User Object Id: " $User.ObjectId
+        $Count++
         $Identity = (Get-EXOMailbox -ExternalDirectoryObjectId $User.ObjectId).Identity
-        write-host "Mailbox Identity "$Identity
+        write-host "Mailbox Identity: "$Identity
         $Auth = Get-CASMailbox -Identity $Identity
         if ($null -eq $Auth.SmtpClientAuthenticationDisabled){
             set-CASMailbox -Identity $Identity -SmtpClientAuthenticationDisabled $true
