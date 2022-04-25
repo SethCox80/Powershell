3eeadd738c800a2875b4e63e65ed45a3d8d0fbc3
Seth Cox <seth.cox80@gmail.com>
2022-04-24 21:27:05 -0400

    Teting Scripts

--- SetSMTPPolicy.ps1
+++ SetSMTPPolicy.ps1
@@ -9,18 +9,18 @@ $Title = "Student"
 $Users = Get-MsolUser
 $Count = 1
 foreach($User in $Users){
-    if($User.Department -eq $Dept){
-        write-host $Count " "$User.Displayname -ForegroundColor Blue
+    #if($User.Department -eq $Dept){
+        write-host $Count $User.Displayname -ForegroundColor Blue
         Write-Host "Department: " $User.Department
         Write-Host "User Object Id: " $User.ObjectId
         $Count++
         $Identity = (Get-EXOMailbox -ExternalDirectoryObjectId $User.ObjectId).Identity
         write-host "Mailbox Identity: "$Identity
         $Auth = Get-CASMailbox -Identity $Identity
-        if ($null -eq $Auth.SmtpClientAuthenticationDisabled){
-            set-CASMailbox -Identity $Identity -SmtpClientAuthenticationDisabled $true
+        if ($Auth.ImapEnabled -eq $true){
+            set-CASMailbox -Identity $Identity -ImapEnabled $false
         }
         $Auth = Get-CASMailbox -Identity $Identity
-        Write-Host $Identity "set to: "$Auth.SmtpClientAuthenticationDisabled -ForegroundColor Green `n
-    }
+        Write-Host $Identity "set to: "$Auth.ImapEnabled -ForegroundColor Green `n
+    #}
 }
---
+++ test.ps1
@@ -0,0 +1,10 @@
+Get-ExecutionPolicy   
+Set-ExecutionPolicyUnrestricted–ScopeCurrentUser  
+$credential = Get-Credential  
+Connect-MsolService–Credential $credential  
+$UserName = Read-Host"Enter the username"  
+$authentication = New-Object-Type Name Microsoft.Online.Administration.StrongAuthenticationRequirement  
+$authentication.RelyingParty = "*"  
+$authentication.State = "Enabled"  
+$authentication.RememberDevicesNotIssuedBefore = (Get-Date)  
+Get-MsolUser-UserPrincipalName$UserName | Set-MsolUser-UserPrincipalName$UserName-StrongAuthenticationRequirements$authentication A
