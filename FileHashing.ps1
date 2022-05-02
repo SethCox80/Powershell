4ab20bbb3e216a15a02a7c28a0242a1cb3b1c1a2
Seth Cox <Seth.Cox80@gmail.com>
2022-05-02 13:00:44 -0400

    Added hash comparison input

--- FileHashing.ps1
+++ FileHashing.ps1
@@ -1,6 +1,14 @@
-$Hashes = ("MD5","SHA1","SHA256", "SHA384", "SHA512")
-$FileAndPath = read-host "Enter Path and File with extendion:"
+$Algs = ("MD5","SHA1","SHA256", "SHA384", "SHA512")
+$FileToTest = "C:\Temp\ubuntu-20.04.4-live-server-amd64.iso"
 
-foreach($Hashes in $Hashes){
-    Get-FileHash -Path $FileAndPath -Algorithm $Hashes
-}
+$ControlHash = read-host "Enter known hash"
+
+write-host "Hash`t`tPath and File" -ForegroundColor Yellow
+foreach($Alg in $Algs){
+    $Hash = (Get-FileHash -Path $FileToTest -Algorithm $Alg).Hash
+    Write-Host "[-]"$Alg "`t[:]" $FileToTest "`t[:]" $Hash | Format-Table
+    if($Hash -eq $ControlHash){
+        Write-Host "[+] Matches"$alg "Hash of" $FileToTest "[:]"$ControlHash -ForegroundColor Green
+    }
+ }
+ 
