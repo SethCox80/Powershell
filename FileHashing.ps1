a9a1fee464fa3ca016c566cb19d412a4ab297c3c
Seth Cox <seth.cox80@gmail.com>
2022-05-02 15:56:55 -0400

    Rewrite first function

--- FileHashing.ps1
+++ FileHashing.ps1
@@ -1,18 +1,3 @@
-function Test-Hash {
-    param (
-        [Parameter][string] $FileToHash,
-        [Parameter][string] $ControlHash 
-    )
-    $Algs = ("MD5", "SHA1", "SHA256", "SHA384", "SHA512")
-    write-host "Hash`t`tPath and File" -ForegroundColor Yellow
-    foreach ($Alg in $Algs) {
-        $Hash = (Get-FileHash -Path $FileToHash -Algorithm $Alg).Hash
-        Write-Host "[-]"$Alg "`t[:]" $FileToHash "`t[:]" $Hash | Format-Table
-        if ($Hash -eq $ControlHash) {
-            Write-Host "[+] Matches"$alg "Hash of" $H "[:]"$ControlHash -ForegroundColor Green
-        }
-    }
-}
 function Get-FileToHash {
     $FileToHash = read-host "Enter path and filename of file to test Hash(s)"
     if (Test-Path $FileToHash) {
@@ -32,8 +17,14 @@ function Get-ControlHash {
         Get-ControlHash
     }
 }
-
 $FileTohash = Get-FileToHash
 $ControlHash = Get-ControlHash
-Test-Hash($FileTohash, $ControlHash)
-
+$Algs = ("MD5", "SHA1", "SHA256", "SHA384", "SHA512")
+write-host "Hash`t`tPath and File" -ForegroundColor Yellow
+foreach ($Alg in $Algs) {
+    $Hash = (Get-FileHash -Path $FileToHash -Algorithm $Alg).Hash
+    Write-Host "[-]"$Alg "`t[:]" $FileToHash "`t[:]" $Hash | Format-Table
+    if ($Hash -eq $ControlHash) {
+        Write-Host "[+] Matches"$alg "Hash of" $FileTohash "[:]"$ControlHash -ForegroundColor Green
+    }
+}
