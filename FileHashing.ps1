f3f4d514239d305032683595d83dfb3a1e079df5
Seth Cox <Seth.Cox80@gmail.com>
2022-05-03 10:27:10 -0400

    Format print strings

--- FileHashing.ps1
+++ FileHashing.ps1
@@ -17,14 +17,18 @@ function Get-ControlHash {
         Get-ControlHash
     }
 }
-$FileTohash = Get-FileToHash
-$ControlHash = Get-ControlHash
+$FileTohash = "C:\Temp\ubuntu-20.04.4-live-server-amd64.iso"  # Get-FileToHash
+$ControlHash =  "28ccdb56450e643bad03bb7bcf7507ce3d8d90e8bf09e38f6bd9ac298a98eaad" # Get-ControlHash
 $Algs = ("MD5", "SHA1", "SHA256", "SHA384", "SHA512")
 write-host "Hash`t`tPath and File" -ForegroundColor Yellow
 foreach ($Alg in $Algs) {
     $Hash = (Get-FileHash -Path $FileToHash -Algorithm $Alg).Hash
-    Write-Host "[-]"$Alg "`t[:]" $FileToHash "`t[:]" $Hash | Format-Table
+    [String]::Format("[-] {0} `t`t {1} `t {2}", $Alg, $FileToHash, $Hash) 
     if ($Hash -eq $ControlHash) {
-        Write-Host "[+] Matches"$alg "Hash of" $FileTohash "[:]"$ControlHash -ForegroundColor Green
+        [String]::Format("[+] Matches {0} Hash of {1} [:] {2}", $Alg, $FileToHash, $Hash)
+        $Match = $true
     }
+}
+if($Match -ne $true){
+    write-host "File did not match known hash in any hash Algorythm!!!"-ForegroundColor Red
 }
