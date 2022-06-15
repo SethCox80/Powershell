ef2c35120a7ad506ad536e8f76dd723b5a357bab
Seth Cox <seth.cox80@gmail.com>
2022-06-15 16:31:23 -0400

    Upload AzureAD_Force_Signout.ps1

---
+++ AzureAD_Force_signout.ps1
@@ -0,0 +1,19 @@
+#Import-Module AzureAD
+#connect-azuread 
+
+$Students = Get-Msoluser | Select-Object Displayname, userprincipalname, Title, Department, BlockCredential
+
+foreach ($Student in $Students) {
+    $Name = $Student.Displayname
+    $Username = $Student.userprincipalname
+    $Grade = $Student.Department
+    $Title = $Student.Title
+    $IsBlocked = $Student.BlockCredential
+
+    If ($Title -eq 'Student') {
+        write-host $Name, "grade" $Grade, "is blocked: " $IsBlocked -ForegroundColor Yellow
+        Get-AzureADUser -SearchString $Name | Revoke-AzureADUserAllRefreshToken
+        Write-Warning "Session Revoked for $Username"
+    }
+            
+}
