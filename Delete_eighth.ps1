4a879578a43a921db40bfab301559eae0f48d3d2
Seth Cox <seth.cox80@gmail.com>
2022-07-05 15:06:46 -0400

    Save all changes for upload

--- Delete_eighth.ps1
+++ Delete_eighth.ps1
@@ -1,14 +1,21 @@
+<#
 
+Incomplete project
 
-$Users  = get-msoluser | Select-object userprincipalname, objectid, title, department
+#>
 
-# write-host $Users
+$Users  = get-msoluser 
+$Eight_Deleted = @()
 
 foreach ($User in $Users){
+    $First = $User.FirstName
+    $Last = $User.LastName
+    $DisplayName = $User.displayname
+    $Title = $User.Title
     $Student = $User.userprincipalname
     $ObjID = $User.objectid
     if ($User.Department -eq 'eighth'){
-        write-host $Student 'will be unlocked' -foregroundcolor Blue
+        write-warning $Student 'will be deleted'
         Set-MsolUser -ObjectID $ObjID -BlockCredential $true
         $IsLocked = (get-msoluser -userprincipalname $Student).blockcredential 
         if ($IsLocked -eq $true){
@@ -17,5 +24,24 @@ foreach ($User in $Users){
         else {
             write-host '--   '$Student 'is still unlocked' -foregroundcolor Red
         }
+        $Eight_Deleted = $Eight_Deleted + [PSCustomObject] @{
+            First_Name          = $First;
+            Last_Name           = $Last;
+            Display_Name        = $DisplayName;
+            User_Principal_Name = $Username;
+            Title               = $Title;
+            Department          = $Department;
+            Grade               = $Grade
+            Is_Blocked          = $IsBlocked
+        }
     }
+    
 }
+
+$TestPath = test-path -path 'c:\temp'
+if ($TestPath -ne $true) {
+    New-Item -ItemType directory -Path 'c:\temp' | Out-Null
+    write-Host  'Creating directory to write file to c:\temp. Your file is uploaded as Users.csv'
+}
+else { Write-Host "Your file has been uploaded to c:\temp as 'Eight_Deleted.csv'" }
+$Eight_Deleted | Export-Csv c:\temp\Eight_Deleted.csv -notypeinformation
